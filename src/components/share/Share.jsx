e3b3b85b5cbe46c9e537c7e5aa22b75e9ede6170
Lynette Wanjiku <lynewan.r@gmail.com>
2023-02-27 17:36:29 +0300

    Functionality on the photo and video icons at the post container

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./share.css";
 
-import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material";
+import { PermMedia,PlayCircleFilledOutlined, Label, Room, EmojiEmotions } from "@mui/icons-material";
 
 const Share = () => {
     return (
@@ -12,27 +12,40 @@ const Share = () => {
                     <input
                         placeholder="What's on your mind Lynette?"
                         className="shareInput"
-                        type="image*"
                     />
                 </div>
-                <hr className="shareHr"/>
+                <hr className="shareHr" />
                 <div className="shareBottom">
                     <div className="shareOptions">
                         <div className="shareOption">
-                        <PermMedia className="shareIcon" id="media"/>
-                        <span className="shareOptionText">Photo or Video</span>
-                        </div> 
-                        <div className="shareOption">
-                        <Label className="shareIcon" id="tag"/>
-                        <span className="shareOptionText">Tag</span>
+                            <PermMedia className="shareIcon" id="photo" />
+                            <label className="fileContainer">
+                                <input type="file" />
+                                <span className="shareOptionText">
+                                    Photo</span>
+                            </label>
+
                         </div>
                         <div className="shareOption">
-                        <Room className="shareIcon" id="location"/>
-                        <span className="shareOptionText">Location</span>
+                            <PlayCircleFilledOutlined className="shareIcon" id="video" />
+                            <label className="fileContainer">
+                                <input type="file" />
+                                <span className="shareOptionText">
+                                    Video</span>
+                            </label>
+
                         </div>
                         <div className="shareOption">
-                        <EmojiEmotions className="shareIcon" id="feelings"/>
-                        <span className="shareOptionText">Feelings</span>
+                            <Label className="shareIcon" id="tag" />
+                            <span className="shareOptionText">Tag</span>
+                        </div>
+                        <div className="shareOption">
+                            <Room className="shareIcon" id="location" />
+                            <span className="shareOptionText">Location</span>
+                        </div>
+                        <div className="shareOption">
+                            <EmojiEmotions className="shareIcon" id="feelings" />
+                            <span className="shareOptionText">Feelings</span>
                         </div>
                     </div>
                     <button className="shareButton">Share</button>
