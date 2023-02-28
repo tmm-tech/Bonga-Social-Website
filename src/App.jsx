d748c5079371c4caae2c078f31b9a895c13a4436
williammunyakathuo <tjwellieyam@gmail.com>
2023-02-28 08:50:42 +0300

    added routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Friends from './pages/Friends';
+import Notification from './pages/Notification';
+import { Routes, Route } from 'react-router-dom';
+import Videos from './pages/Videos';
 
 function App() {
   return (
     <div className="App">
-      <Friends />
-
+      <Routes>
+        <Route path='/' element={<Friends/>} ></Route>
+        <Route path='/notification' element={<Notification/>} ></Route>
+        <Route path='/videos' element={<Videos/ >} ></Route>
+      </Routes>
       
     </div>
   );
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,23 @@
+import Followers from "../components/followers";
+import Shortcuts from "../components/Shortcuts";
+import Videogrid from "../components/Videogrid";
+
 const Videos = () => {
-    return ( 
-        
-     );
+    return (
+        <div className="videos">
+            <div className="maincontainer">
+                <div className="cont1">
+                    <Shortcuts/>
+                </div>
+                <div className="cont2">
+                    <Videogrid />
+                </div>
+                <div className="cont3">
+                    <Followers />
+                </div>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Videos;
